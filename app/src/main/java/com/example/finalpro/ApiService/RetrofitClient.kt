8812c84cf887e983b59eaed6de1d1edf86f1aaf7
shaik <shaikhaman3588@gmail.com>
2024-08-11 23:32:08 +0530

    Initial Commit

--- app/src/main/java/com/example/finalpro/ApiService/RetrofitClient.kt
+++ app/src/main/java/com/example/finalpro/ApiService/RetrofitClient.kt
@@ -5,14 +5,10 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-    private const val BASE_URL = "http://<your_server_ip>:3000/"
+    private const val BASE_URL = "http://192.168.1.2:5000/api/"
 
-    val instance: ApiService by lazy {
         val retrofit = Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
-
-        retrofit.create(ApiService::class.java)
-    }
 }
